f8781148937a94448cf33c468f975c8948895f86
dpryan79 <dpryan@dpryan.com>
2014-12-01 13:45:14 +0100

    Sort a heap before outputting it

--- alignmentHeap.c
+++ alignmentHeap.c
@@ -12,6 +12,27 @@ alignmentHeap * alignmentHeap_init(int size) {
     return ret;
 }
 
+int alignmentHeapSortFunc(const void *pa, const void *pb) {
+    bam1_t *a = *((bam1_t **) pa);
+    bam1_t *b = *((bam1_t **) pb);
+    int rv = 0;
+
+    if(a->core.tid == b->core.tid) {
+        if(a->core.pos == b->core.pos) {
+            if(bam_endpos(a) != bam_endpos(b)) rv = bam_endpos(a) - bam_endpos(b);
+        } else {
+            rv = a->core.pos - b->core.pos;
+        }
+    } else {
+        rv = a->core.tid - b->core.tid;
+    }
+    return rv;
+}
+
+void sortHeap(alignmentHeap *heap) {
+    qsort((void *) heap->heap, heap->l, sizeof(bam1_t*), &alignmentHeapSortFunc);
+}
+
 //Export
 void alignmentHeap_destroy(alignmentHeap *heap) {
     int i;
@@ -23,6 +44,7 @@ void alignmentHeap_destroy(alignmentHeap *heap) {
 //Export
 void writeHeap(samFile *of, bam_hdr_t *hdr, alignmentHeap *heap) {
     int i;
+    sortHeap(heap);
     for(i=0; i<heap->l; i++) {
         sam_write1(of, hdr, heap->heap[i]);
         bam_destroy1(heap->heap[i]);
@@ -32,7 +54,7 @@ void writeHeap(samFile *of, bam_hdr_t *hdr, alignmentHeap *heap) {
 //Export
 alignmentHeap * writeHeapUntil(samFile *of, bam_hdr_t *hdr, alignmentHeap *heap, int depth) {
     int cur_i = 0;
-    bam1_t *b = heap->heap[0];
+    bam1_t *b;
     int32_t lpos = -1, newl = 0;
     InDel reg;
 
@@ -48,6 +70,9 @@ alignmentHeap * writeHeapUntil(samFile *of, bam_hdr_t *hdr, alignmentHeap *heap,
         heap->l = 0;
         return heap;
     }
+
+    sortHeap(heap);
+    b = heap->heap[0];
     reg.tid = b->core.tid;
     reg.start = b->core.pos;
     reg.end = bam_endpos(b)-1;
@@ -83,7 +108,9 @@ alignmentHeap * writeHeapUntil(samFile *of, bam_hdr_t *hdr, alignmentHeap *heap,
         reg.end = bam_endpos(b)+1;
     }
     //Transfer the remainder over
-    for(;cur_i < heap->l; cur_i++) heap->heap[newl++] = heap->heap[cur_i];
+    for(;cur_i < heap->l; cur_i++) {
+        heap->heap[newl++] = heap->heap[cur_i];
+    }
     heap->l = newl;
 #ifdef DEBUG
     fprintf(stderr, "[writeHeapUntil] Returning a heap of length %" PRId32 "\n", newl); fflush(stderr);
